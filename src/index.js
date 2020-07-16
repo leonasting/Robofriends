d9709591eea7b91b91c240403b809e6cf9d9f37b
Alavi Khan <Alavi.Khan@sapiens.com>
2020-07-16 22:10:12 +0530

    16_7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider } from 'react-redux';
-import {createStore } from 'redux';
+import {createStore,applyMiddleware } from 'redux';
 
 import './index.css';
 
@@ -9,7 +9,11 @@ import App from './container/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import {searchRobots} from './reducer.js';
-const store = createStore(searchRobots) ;
+import {createLogger} from 'redux-logger';
+const logger= createLogger();
+const store = createStore(searchRobots,applyMiddleware(logger)) ;
+
+
 ReactDOM.render(
   <React.StrictMode>
 
